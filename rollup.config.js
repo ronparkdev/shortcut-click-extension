db876f9e4c3033f136d160c5ca474b363e12d3b6
Ron Park <ronpark.dev@gmail.com>
2024-05-29 01:47:20 +0900

    config: Fix terser import error

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import replace from '@rollup/plugin-replace'
 import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from '@rollup/plugin-terser'
+import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
